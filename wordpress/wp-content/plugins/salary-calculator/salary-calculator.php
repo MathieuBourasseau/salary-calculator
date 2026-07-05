2dfba598f7c24a42f447913b5a47026d30bbedf0
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-07-05 17:17:33 +0200

    feat: folder plugins created + folder salary-calculator created + salary-calculator.php file created

---
+++ wordpress/wp-content/plugins/salary-calculator/salary-calculator.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Salarium - Calculateur de salaire
+ * Description: Affiche un calculateur brut/net via le shortcode [salary_calculator].
+ * Version: 1.0
+ * Author: Mathieu Bourasseau
+ */
