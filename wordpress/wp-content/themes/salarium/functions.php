d4d52d50cf64156ef79955af3db273a8753fa827
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-07-05 16:31:28 +0200

    feat: salarium_enqueue_assets function created

--- wordpress/wp-content/themes/salarium/functions.php
+++ wordpress/wp-content/themes/salarium/functions.php
@@ -1,1 +1,25 @@
 <?php
+
+// This function allows to load the style and js script
+function salarium_enqueue_assets() {
+
+    // Style from tailwind
+    wp_enqueue_style(
+        'salarium-style',                                    
+        get_template_directory_uri() . '/dist/output.css',    
+        array(),                                             
+        '1.0'                                                 /
+    );
+
+    // Js
+    wp_enqueue_script(
+        'salarium-script',
+        get_template_directory_uri() . '/js/main.js',
+        array(),
+        '1.0',
+        true 
+    );
+}
+
+// The function is called when the hook wp_enqueue_scripts is activated 
+add_action('wp_enqueue_scripts', 'salarium_enqueue_assets');
